36f7ad4ccefe44b21b5e7dace07e4a4a0c1397db
martynov007 <k.v.martynov@student.khai.edu>
2021-09-30 21:31:10 +0300

    ChatBot 1-st stage

---
+++ ChatBot/chatbot.py
@@ -0,0 +1,7 @@
+import datetime
+
+BOT_NAME = "CHARLIE"
+CURRENT_YEAR = datetime.date().year
+
+if __name__ == "__main__":
+    print(f"Hello! My name is {BOT_NAME}\nI was created in {CURRENT_YEAR}", 1)
