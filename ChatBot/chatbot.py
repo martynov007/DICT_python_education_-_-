b5baf9ba2724f7edc041e59aa65e7aa0c604ec67
martynov007 <k.v.martynov@student.khai.edu>
2021-09-30 21:40:07 +0300

    ChatBot 2-nd stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -1,7 +1,10 @@
-import datetime
+from datetime import date
 
 BOT_NAME = "CHARLIE"
-CURRENT_YEAR = datetime.date().year
+CURRENT_YEAR = date.today().year
 
 if __name__ == "__main__":
-    print(f"Hello! My name is {BOT_NAME}\nI was created in {CURRENT_YEAR}", 1)
+    print(f"Hello! My name is {BOT_NAME}\nI was created in {CURRENT_YEAR}")
+    print("Please, remind me your name.")
+    username = input()
+    print(f"What a great name you have, {username}!")
