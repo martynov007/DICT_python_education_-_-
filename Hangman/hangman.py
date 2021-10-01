ef36338d5b428ad2263a825ccb1c0fc2e386ef87
martynov007 <k.v.martynov@student.khai.edu>
2021-10-02 00:10:17 +0300

    Hangman stage 2

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,3 +1,13 @@
 if __name__ == '__main__':
     # stage 1
     print('HANGMAN\nTry to not being hanged')
+
+    # stage 2
+    TO_GUESS = 'witch'
+    print('Guess the word:')
+    user_guess = input()
+
+    if user_guess == TO_GUESS:
+        print('You\'r survived!')
+    else:
+        print('You\'r hanged!')
