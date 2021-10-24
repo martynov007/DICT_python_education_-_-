5f967e46b537d7673b1b54b2c58e752c953d3af2
martynov007 <k.v.martynov@student.khai.edu>
2021-10-24 22:39:12 +0300

    TicTacToe stage2

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,8 +1,19 @@
 def main():
-    print('XOX')
-    print('XXX')
-    print('OXO')
+    cells = input('Enter cells:')
 
+    if len(cells) != 9:
+        print('Only 9 symbols')
+        return None
+    # * проверка вхождения лишних букв в строку
+    elif g:=list(filter(lambda x: x not in ['X', 'O', '_'], cells)):
+        print(', '.join(g) + ' shouldn\'t be there')
+        return None
+
+    print('-------')
+    print('|' + ' '.join(list(cells[0:3])) + '|')
+    print('|' + ' '.join(list(cells[3:6])) + '|')
+    print('|' + ' '.join(list(cells[6:9])) + '|')
+    print('-------')
 
 if __name__ == "__main__":
     main()
