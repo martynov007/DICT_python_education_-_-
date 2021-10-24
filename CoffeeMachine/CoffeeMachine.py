7d92394af696b8def7acc24cfa50081997eabc70
martynov007 <k.v.martynov@student.khai.edu>
2021-10-24 18:43:07 +0300

    stage1

---
+++ CoffeeMachine/CoffeeMachine.py
@@ -0,0 +1,12 @@
+import logging
+
+logging.basicConfig(level=logging.INFO,format='%(message)s')
+
+# * stage1
+logging.info('Starting to make a coffee')
+logging.info('Grinding coffee beans')
+logging.info('Boiling water')
+logging.info('Mixing boiled water with crushed coffee beans')
+logging.info('Pouring coffee into the cup')
+logging.info('Pouring some milk into the cup')
+logging.info('Coffee is ready!')
