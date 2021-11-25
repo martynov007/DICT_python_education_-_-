e78af793b456f1b1bd47f8dd573a8b4b4e5c3882
martynov007 <k.v.martynov@student.khai.edu>
2021-11-25 23:19:42 +0200

    stage4

--- CreditCalculator/CreditCalculator.py
+++ CreditCalculator/CreditCalculator.py
@@ -40,16 +40,46 @@ class CreditCalculator:
         )
 
 
-class ImprovedCreditCalculator:
-    def calc_montly_payment(self, n, year_interest_rate, P):
-        i = year_interest_rate / (100 * 12)
-        self.A = P * ((i * pow(1 + i, n))/(pow(1 + i, n) - 1))
-        print(f'Your monthly payment = {self.A}!')
+class AnnuityCreditCalculator:
+    def __init__(self, rate = None, A = None, n = None, P = None) -> None:
+        self.rate = rate or 0
+        self.A = A or 0
+        self.n = n or 0
+        self.P = P or 0
         
-        
-    def calc_monthes_amount(self, A, year_interest_rate, P):
-        i = year_interest_rate / (100 * 12)
-        self.n = math.ceil(math.log(A/(A - i * P), 1 + i))
+        if self.rate < 0\
+            or self.P < 0\
+                or self.n < 0\
+                    or self.A < 0:
+            print('Incorrect parameters')
+            return None
+
+        if rate and A and n:
+            self.calc_credit_amount()
+            self.overpayment()
+            return
+        if rate and A and P:
+            self.calc_monthes_amount()
+            self.overpayment()
+            return
+        if n and rate and P:
+            self.calc_montly_payment()
+            self.overpayment()
+            return 
+        else:
+            print('Incorrect parametres')
+            return
+
+
+    def calc_montly_payment(self):
+        i = self.rate / (100 * 12)
+        self.A = self.P * ((i * pow(1 + i, self.n))/(pow(1 + i, self.n) - 1))
+        print(f'Your monthly payment = {math.ceil(self.A)}!')
+
+
+    def calc_monthes_amount(self):
+        i = self.rate / (100 * 12)
+        self.n = math.ceil(math.log(self.A/(self.A - i * self.P), 1 + i))
 
         years = math.floor(self.n / 12)
         months = math.ceil(self.n % 12)
@@ -62,36 +92,84 @@ class ImprovedCreditCalculator:
         elif months > 0:
             print(f'It will take {months} month(s) to repay this loan!')
 
-    def calc_credit_amount(self, A, year_interest_rate, n):
-        i = year_interest_rate / (100 * 12)
-        self.P = A / ((i * pow(1 + i, n))/(pow(1+i, n) - 1))
 
-        print(f'Your loan principal = {self.P}!')
-        
+    def calc_credit_amount(self):
+        i = self.rate / (100 * 12)
+        self.P = self.A / ((i * pow(1 + i, self.n))/(pow(1+i, self.n) - 1))
+
+        print(f'Your loan principal = {round(self.A)}!')
+
+
+    def overpayment(self):
+        print(f'Overpayment = {math.ceil((self.A * self.n) - self.P)}')
+
+
+class DiffCreditCalculator():
+    def __init__(self, rate = None, A = None, n = None, P = None) -> None:
+        self.rate = rate or 0
+        self.P = P or 0
+        self.n = n or 0
+        self.sum_overpayment = 0
+
+        if A:
+            print('Incorrect parameters')
+            return None
+
+        if self.rate <= 0\
+                or self.n <= 0\
+                    or self.P <= 0:
+            print('Incorrect parameters')
+            return None
+
+        if rate and P and n:
+            self.calc_credit_amount()
+        else:
+            print('Incorrect parametres')
+            return
+
+        self.overpayment()
+
+
+    def calc_credit_amount(self):
+        for m in range(1, self.n+1):
+            i = self.rate / (100 * 12)
+            self.D = (self.P / self.n) + i * (self.P - (self.P * (m - 1)/self.n))
+
+            self.sum_overpayment += self.D
+
+            print(f'Month {m}: payment is {math.ceil(self.D)}!')
+
+
+    def overpayment(self):
+        print(f'Overpayment = {math.ceil(self.sum_overpayment - self.P)}')
+
 
 def main():
-    improved_credit_calc = ImprovedCreditCalculator()
-    choice = input(('What do you want to calculate?'
-                    '\ntype "n" for number of monthly payments,'
-                    '\ntype "a" for annuity monthly payment amount,'
-                    '\ntype "p" for loan principal:'))
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--type', type=str)
+    parser.add_argument('--interest', type=float)
+    parser.add_argument('--principal', type=float)
+    parser.add_argument('--payment', type=float)
+    parser.add_argument('--periods', type=int)
+    args = parser.parse_args('--type=diff --principal=500000 --periods=8 --interest=7.8'.split())
+
+    choice = args.type
+    A = args.payment
+    rate = args.interest
+    P = args.principal
+    n = args.periods
+    
+    if not choice:
+        print('Incorrect parameters')
+        return
 
     match choice:
-        case 'n':
-            P = float(input('Enter the loan principal: '))
-            A = float(input('Enter the monthly payment: '))
-            rate = float(input('Enter the loan interest: '))
-            improved_credit_calc.calc_monthes_amount(A, rate, P)
-        case 'p':
-            A = float(input('Enter the monthly payment: '))
-            n = float(input('Enter the number of periods: '))
-            rate = float(input('Enter the loan interest: '))
-            improved_credit_calc.calc_credit_amount(A, rate, n)
-        case 'a':
-            P = float(input('Enter the loan principal: '))
-            n = float(input('Enter the number of periods: '))
-            rate = float(input('Enter the loan interest: '))
-            improved_credit_calc.calc_montly_payment(n, rate, P)
+        case 'diff':
+            DiffCreditCalculator(rate, A, n, P)
+        case 'annuity':
+            AnnuityCreditCalculator(rate, A, n, P)
         case _:
             print('not option for {}'.format(choice))
 
