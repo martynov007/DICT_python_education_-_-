b930fa2fed83320a586dd42f8d0cd177ae6ac996
martynov007 <k.v.martynov@student.khai.edu>
2022-04-19 12:52:07 +0300

    stage2

--- currency-exchange/currency-exchange.py
+++ currency-exchange/currency-exchange.py
@@ -1,11 +1,19 @@
 class CurrencyExchange:
+    exchange_rates = {
+        'USD': 2.5,
+        'UAH': 0.8,
+        'RUB': 1000,
+        'EUR': 2
+    }
+
     def __init__(self) -> None:
         self.currency = float(input('Please enter your currency amount: '))
-        exchange_rate = float(input('Enter exchange rate to USD for your currency: '))
-        self.exchange(exchange_rate)
+        # exchange_rate = float(input('Enter exchange rate to USD for your currency: '))
+        for key, value in self.exchange_rates.items():
+            self.exchange(key, value)
 
-    def exchange(self, rate):
-        print('You will get {} dollars'.format(self.currency * rate))
+    def exchange(self, exchange_currency, rate):
+        print('You will get {} {}'.format(self.currency * rate, exchange_currency))
 
 if __name__ == "__main__":
     CurrencyExchange()
