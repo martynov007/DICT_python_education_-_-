0577863ff3ce27d3fbb356077b1dadcc4e4284ab
martynov007 <k.v.martynov@student.khai.edu>
2022-04-20 14:47:42 +0300

    stage 3

--- currency-exchange/currency-exchange.py
+++ currency-exchange/currency-exchange.py
@@ -1,16 +1,25 @@
-class CurrencyExchange:
-    exchange_rates = {
-        'USD': 2.5,
-        'UAH': 0.8,
-        'RUB': 1000,
-        'EUR': 2
-    }
+import json
+import requests
 
+
+class CurrencyExchange:
     def __init__(self) -> None:
-        self.currency = float(input('Please enter your currency amount: '))
+        self.currency = input('Please enter your currency: ')
+        self.currency_amount = float(input('Please enter your currency amount: '))
+        currency_rate_res = requests.get('http://www.floatrates.com/daily/{}.json'.format(self.currency))
+
+        if not currency_rate_res:
+            print('Cant find that currency')
+            return
+
+        currency_rate_js = currency_rate_res.json()
+
+        print('EUR: {}'.format(round(self.currency_amount * currency_rate_js['eur']['rate'], 2)))
+        print('USD: {}'.format(round(self.currency_amount * currency_rate_js['usd']['rate'],2)))
+
         # exchange_rate = float(input('Enter exchange rate to USD for your currency: '))
-        for key, value in self.exchange_rates.items():
-            self.exchange(key, value)
+        # for key, value in self.exchange_rates.items():
+        #     self.exchange(key, value)
 
     def exchange(self, exchange_currency, rate):
         print('You will get {} {}'.format(self.currency * rate, exchange_currency))
