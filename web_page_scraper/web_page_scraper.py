887b2ba6d56005384aebd29851a85a9db4f430af
martynov007 <k.v.martynov@student.khai.edu>
2022-04-30 16:21:26 +0300

    stage2

--- web_page_scraper/web_page_scraper.py
+++ web_page_scraper/web_page_scraper.py
@@ -1,20 +1,41 @@
+import json
+from pydoc import describe
 import requests as req
+from bs4 import BeautifulSoup as bs
+
 
 class WebPageScraper:
+    
     def __init__(self) -> None:
+        headers = {'Accept-Language': 'en-US,en;q=0.5'}
         url = input('Enter your url:')
-        res = req.get(url)
 
+        res = req.get(url, headers=headers)
+        
         if not res:
             print('Invalid resource!')
             return
+
+        soup = bs(res.text, 'html.parser')
+
+        title_html = soup.find('title')
+        script_dict = json.loads(soup.find('script', type='application/ld+json').string)
         
-        if not res_js.get('content'):
-            print('Resource dont have a quote!')
+        if not title_html or not script_dict:
+            print('Invalid resource!')
             return
 
-        res_js = res.json()
-        print(res_js['content'])
+        self.show_film_dict(title_html.text, script_dict.get('description'))
+
+    @staticmethod
+    def show_film_dict(title, description):
+        film_dict = {
+            'title': title,
+            'description': description
+        }
+
+        print(film_dict)
+
 
 if __name__ == "__main__":
     WebPageScraper()
