017824cb2dcafda2ceb8c7f49dbfe49fd04a5848
martynov007 <k.v.martynov@student.khai.edu>
2022-05-06 23:15:43 +0300

    stage 5

--- web_page_scraper/web_page_scraper.py
+++ web_page_scraper/web_page_scraper.py
@@ -1,54 +1,69 @@
+from genericpath import exists
 import string
 import requests as req
 import os
 from bs4 import BeautifulSoup as bs
-
+import urllib.parse 
 
 class WebPageScraper:
     
     def __init__(self) -> None:
         headers = {'Accept-Language': 'en-US,en;q=0.5'}
-        url = input('Enter your url:')
+        pages_count = int(input('Enter pages count:'))
+        article_type = input('Enter article type:')
         base_url = 'https://www.nature.com'
-        res = req.get(url, headers=headers)
-        
-        if not res:
-            print('The URL returned {}'.format(res.status_code))
-            return
+        articles_url = urllib.parse.urljoin(base_url, 'nature/articles')
+        params = {
+                'sort': 'PubDate',
+                'year': 2022,
+                'page': 0,
+            }
 
-        soup = bs(res.text, 'html.parser')
-
-        data_tests = soup.find_all('span', {'data-test':'article.type'})
-        data_track_actions = soup.find_all('a', {'data-track-action': "view article"})
-        
-        saved_articles = []
-
-        for dt, dta in zip(data_tests,data_track_actions):
-            article = dt.get_text().strip('\n').lower()
-
-            if article != 'news':
-                continue
-
-            article_res = req.get(base_url + dta['href'], headers=headers)
-
-            if not article_res:
-                print('Invalid resource!Cant get body and title for this article')
-                continue
+        for pg_nmbr in range(1, pages_count+1):
+            try:
+                os.mkdir(f'./web_page_scraper/Page_{pg_nmbr}')
+            except OSError:
+                pass
             
-            soup2 = bs(article_res.text, 'html.parser')
-            translator = str.maketrans('','',string.punctuation)
-            prep_title = str.translate(soup2.title.text, translator)
-            prep_title = prep_title.replace(' ', '_')
+            params['page'] = pg_nmbr
+            res = req.get(articles_url, headers=headers, params=params)
 
-            with open(os.path.join('web_page_scraper',f'{prep_title}.html'), mode='wb') as f:
-                article_body = soup2.body
-                wrapped_artl_body = article_body.wrap(soup2.new_tag("div"))
-                encoded_body = wrapped_artl_body.encode('utf-8')
+            soup = bs(res.text, 'html.parser')
 
-                f.write(encoded_body)
-                saved_articles.append(prep_title)
+            data_tests = soup.find_all('span', {'data-test':'article.type'})
+            data_track_actions = soup.find_all('a', {'data-track-action': "view article"})
+            
+            saved_articles = []
 
-        print('Saved articles: ', saved_articles)
+            for dt, dta in zip(data_tests,data_track_actions):
+                article = dt.get_text().strip('\n').lower()
+
+                if article != article_type.lower():
+                    continue
+
+                article_res = req.get(base_url + dta['href'], headers=headers)
+
+                if not article_res:
+                    print('Invalid resource!Cant get body and title for this article')
+                    continue
+                
+                soup2 = bs(article_res.text, 'html.parser')
+                translator = str.maketrans('','',string.punctuation)
+                prep_title = str.translate(soup2.title.text, translator)
+                prep_title = prep_title.replace(' ', '_')
+
+                with open(os.path.join('web_page_scraper',f'Page_{pg_nmbr}',f'{prep_title}.html'), mode='wb', ) as f:
+                    article_body = soup2.body.find_all('p')
+                    all_text = ''.join([x.text for x in article_body])
+                    new_tag = soup2.new_tag("div")
+                    new_tag.string = all_text
+
+                    encoded_body = new_tag.encode('utf-8')
+
+                    f.write(encoded_body)
+                    saved_articles.append(prep_title)
+
+        print('Saved all articles')
 
 if __name__ == "__main__":
     WebPageScraper()
